98acf5f86639e18350868e2cd0d4b0386bd65532
Cadu Espindola <cadu.espindola@gmail.com>
2018-05-18 15:13:23 -0300

    Core\Db

--- src/Core/src/Db/Sql/Select.php
+++ src/Core/src/Db/Sql/Select.php
@@ -1,14 +1,30 @@
 <?php
-
-use Zend\Db\Sql\Select as SqlSelect;
-
 /**
- * Description
+ * Core (http://)
+ * @authors Cadu Espindola (cadu.espindola@gmail.com)
+ * @date    2018-05-18 13:43:49
+ * @version 1.0.0
  */
-class Select extends SqlSelect {
+
+namespace Core\Db\Sql;
+
+use Zend\Db\Sql;
+use Zend\Db\Sql\Where;
+use Zend\Db\Sql\Join;
+use Zend\Db\Sql\Having;
+use Zend\Db\Sql\TableIdentifier;
+
+class Select extends Sql\Select
+{
     
-    function __construct() {
-        
+    /**
+     * Constructor
+     * @param  null|string|array|TableIdentifier $table
+     */
+    public function __construct($table = null)
+    {
+        parent::__construct($table);
+        $this->where = new Where;
     }
 
 }
---
+++ src/Core/src/Db/Sql/Where.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHP Core (http://)
+ * @authors Cadu Espindola (cadu.espindola@gmail.com)
+ * @date    2018-05-18 13:43:49
+ * @version 1.0.0
+ */
+
+namespace Core\Db\Sql;
+
+use Zend\Db\Sql;
+
+class Where extends Sql\Where
+{
+    
+}
